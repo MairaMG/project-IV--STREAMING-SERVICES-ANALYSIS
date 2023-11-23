13d9d6ed6ee9fe5b2ee02d440c540cf01676af3d
Maira Gutierrez <mairagutierrez@Mairas-MacBook-Air.local>
2023-11-23 10:42:42 +0100

    done without ReadMe

---
+++ data/netflix_querys.sql
@@ -0,0 +1,5 @@
+CREATE database IF NOT EXISTS netflix;
+CREATE database IF NOT EXISTS netflix_top_ten;
+CREATE database IF NOT EXISTS netflix_finances;
+CREATE database IF NOT EXISTS netflix_subscribers;
+CREATE database IF NOT EXISTS netflix_originals;
